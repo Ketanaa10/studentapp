dae9d762772ca00937567fed74b054526a67cd5c
Ketanaa10 <ketanaa.a10@gmail.com>
2026-04-28 14:10:28 +0530

    Add files via upload

---
+++ studentapp/src/test/java/com/klu/StudentAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+    @Test
+    void contextLoads() {
+        // Just checks the app starts without errors
+    }
+}
